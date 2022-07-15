675ae4b463571f4b4e2b63ceedc6491db4467a4f
Amogh2409 <amoghsaxena24@gmail.com>
2022-07-15 19:14:12 +0530

    test deployment

--- Myblog/settings.py
+++ Myblog/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^bf4fxzmuzg_$kl#m))d_aqvyy9%sv@(foujv4yk0=e(fvm4(5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','myblog-24.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
