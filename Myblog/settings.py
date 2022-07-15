dedd4e8bece8b47919d71fa6655cb4cf5941db72
Amogh2409 <amoghsaxena24@gmail.com>
2022-07-15 18:29:18 +0530

    Update settings.py

--- Myblog/settings.py
+++ Myblog/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-^bf4fxzmuzg_$kl#m))d_aqvyy9%sv@(foujv4yk0=e(fvm4(5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1','myblog-24.herokuapp.com']
 
 
 # Application definition
