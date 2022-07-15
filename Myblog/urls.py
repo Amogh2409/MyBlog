5ae394243ef54de4932603f7df77274d83d36209
Amogh2409 <amoghsaxena24@gmail.com>
2022-07-15 21:29:23 +0530

    test deployment

--- Myblog/urls.py
+++ Myblog/urls.py
@@ -18,16 +18,17 @@ from django.urls import path
 from django.urls import include
 # from django.views.static import serve
 # from django.conf.urls import url
-# from django.conf import settings
+from django.conf import settings
+from django.conf.urls.static import static
 admin.site.site_header = "MyBlog Admin"
 admin.site.site_title = "MyBlog Admin Panel"
 admin.site.index_title = "Welcome to MyBlog Admin Panel"
-
+ 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('blog/', include('blog.urls')),
     # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
